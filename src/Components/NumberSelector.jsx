3c6b3bbd47010fae133c14b8a638064a7d2dfa52
syedDayim <dayim1277@gmail.com>
2024-04-08 15:16:05 +0530

    select Number componenet Done

--- src/Components/NumberSelector.jsx
+++ src/Components/NumberSelector.jsx
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/NumberSelector.css';
 export default function NumberSelector() {
+  const [selectedNumber, setSelectedNumber] = useState()
   const numbers = [1, 2, 3, 4, 5, 6]
+
+
+  const selectedStyle = {
+    backgroundColor: "black",
+    color: "white",
+  }
+  const unSelectedStyle = {
+    backgroundColor: "white",
+    color: "black",
+  }
+
+  
+
+console.log(selectedNumber)
+
   return (
     <>
       <div className="comp-holder">
 
-      <div className="box-container">
+        <div className="box-container">
+          {
 
-        {numbers.map((value, i) => {
-          return <div className='box' key={i}>{value}</div>
-        })}
-      </div>
-      <p className='paragraph'>Select Number</p>
+            numbers.map((value, i) => {
+              return (<div
+                className='box' key={i}
+                onClick={() => setSelectedNumber(value)}
+                style={(selectedNumber === value) ? selectedStyle : unSelectedStyle}
+              >
+                {value}
+              </div>
+
+              )
+            })
+
+          }
         </div>
+
+
+        <p className='paragraph'>Select Number</p>
+
+      </div>
     </>
   )
 }
